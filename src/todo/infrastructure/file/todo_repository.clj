6c1265c247a371028d56fea9a36b5da7c7aa0987
mgryszko <marcin@grysz.com>
2015-01-18 22:10:50 +0100

    Update todo

--- src/todo/infrastructure/file/todo_repository.clj
+++ src/todo/infrastructure/file/todo_repository.clj
@@ -1,5 +1,6 @@
 (ns todo.infrastructure.file.todo-repository
-  (require [clojure.java.io :as io]))
+  (require [clojure.java.io :as io])
+  (import [java.io RandomAccessFile]))
 
 (def file-name "todo.txt")
 
@@ -8,12 +9,28 @@
 (defn file-exists [] (.exists (io/as-file file-name)))
 
 (defn count-lines []
+  (with-open [r (io/reader file-name)]
+    (count (line-seq r))))
+
+(defn read-all-lines []
   (with-open [rdr (io/reader file-name)]
-             (count (line-seq rdr))))
+    (into [] (line-seq rdr))))
+
+(defn modify-nth-line [lines todo]
+  (let [line-number (:id todo)] 
+     (assoc lines (- line-number 1) (:task todo))))
 
 (defn last-id []
   (if (file-exists) (count-lines) 0))
 
 (defn save-todo! [todo] 
-  (spit "todo.txt" (str (:task todo) line-separator) :append true)
-        (conj todo {:id (last-id)}))
+  (spit "todo.txt" (str (:task todo) line-separator) :append true) (conj todo {:id (last-id)}))
+
+(defn update-todo! [todo]
+  (let [line-number (:id todo)
+        lines (modify-nth-line (read-all-lines) todo)] 
+    (with-open [w (io/writer file-name)]
+       (doseq [line lines]
+         (.write w line)
+         (.newLine w))))
+  todo)
--- test/todo/infrastructure/file/todo_repository_test.clj
+++ test/todo/infrastructure/file/todo_repository_test.clj
@@ -1,13 +1,13 @@
-  (ns todo.infrastructure.file.todo-repository-test
-    (:require [clojure.java.io :as io]
-              [clojure.string :as str]
-              [midje.sweet :refer :all]
-              [todo.infrastructure.file.todo-repository :refer :all]))
+(ns todo.infrastructure.file.todo-repository-test
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [midje.sweet :refer :all]
+            [todo.infrastructure.file.todo-repository :refer :all]))
 
-  (defn delete-todo-file []
+(defn delete-todo-file []
     (io/delete-file "todo.txt" true))
 
-  (defn get-lines [file]
+(defn get-lines [file]
   (str/split-lines (slurp file)))
 
 (defchecker file-saved [expected-todos]
@@ -15,13 +15,32 @@
                     (= (get-lines "todo.txt") expected-todos)))
 
 (background (before :facts (delete-todo-file))
-             (after :facts (delete-todo-file)))
+            (after :facts (delete-todo-file)))
 
 (facts "todo file repository"
   (fact "saves single todo"
-    (save-todo! {:task "any task"}) => (every-checker (file-saved ["any task"])
-                                                      {:id 1 :task "any task"}))
+    (save-todo! {:task "any"}) =>
+      (every-checker (file-saved ["any"])
+                     {:id 1 :task "any"}))
+
   (fact "saves two todos"
-    (save-todo! {:task "first task"})
-    (save-todo! {:task "second task"}) => (every-checker (file-saved ["first task", "second task"])
-                                                         {:id 2 :task "second task"})))
+    (save-todo! {:task "first"})
+    (save-todo! {:task "second"}) =>
+      (every-checker (file-saved ["first", "second"])
+                                 {:id 2 :task "second"}))
+  (against-background 
+    [(before :facts ((save-todo! {:task "first"})
+                     (save-todo! {:task "second"})
+                     (save-todo! {:task "third"})))]
+      
+    (fact "updates first todo"
+      (let [todo {:id 1 :task "updated"}]
+        (update-todo! todo) =>
+          (every-checker (file-saved ["updated" "second" "third"])
+                         {:id 1 :task "updated"})))
+
+    (fact "updates last todo"
+      (let [todo {:id 3 :task "updated"}]
+        (update-todo! todo) =>
+          (every-checker (file-saved ["first" "second" "updated"])
+                         {:id 3 :task "updated"})))))
