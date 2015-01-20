d2d5e2a75cd7bc044b393c4c40142f5b299e330c
mgryszko <marcin@grysz.com>
2015-01-20 22:09:11 +0100

    All file writes are atomic

---
+++ src/todo/infrastructure/file/operations.clj
@@ -0,0 +1,27 @@
+(ns todo.infrastructure.file.operations
+  (:require [clojure.java.io :refer [as-file reader writer]])
+  (:import [java.nio.file Paths Files CopyOption StandardCopyOption]))
+
+(defn file-exists [f] (.exists (as-file f)))
+
+(defn count-lines [f]
+  (with-open [r (reader f)]
+    (count (line-seq r))))
+
+(defn read-lines [f]
+  (if (file-exists f)
+    (with-open [r (reader f)]
+      (into [] (line-seq r)))
+    []))
+
+(defn write-lines [f lines]
+  (with-open [w (writer f)]
+     (doseq [line lines]
+       (.write w line)
+       (.newLine w))))
+
+(defn atomic-move [src dest]
+    (Files/move (.toPath (as-file src)) 
+                (.toPath (as-file dest)) 
+                (into-array CopyOption [StandardCopyOption/ATOMIC_MOVE])))
+
--- src/todo/infrastructure/file/todo_repository.clj
+++ src/todo/infrastructure/file/todo_repository.clj
@@ -1,25 +1,11 @@
 (ns todo.infrastructure.file.todo-repository
-  (require [clojure.java.io :as io])
-  (import [java.io RandomAccessFile]))
+  (:require [todo.infrastructure.file.operations :refer :all])
+  (:import [java.io File]))
 
 (def file-name "todo.txt")
 
-(def line-separator (System/getProperty "line.separator"))
-
-(defn file-exists [] (.exists (io/as-file file-name)))
-
-(defn count-lines []
-  (with-open [r (io/reader file-name)]
-    (count (line-seq r))))
-
-(defn read-all-lines []
-  (if (file-exists)
-    (with-open [rdr (io/reader file-name)]
-      (into [] (line-seq rdr)))
-    []))
-
 (defn next-id []
-  (+ (if (file-exists) (count-lines) 0) 1))
+  (+ (if (file-exists file-name) (count-lines file-name) 0) 1))
 
 (defn modify-nth-line [lines todo]
   (let [line-number (:id todo)] 
@@ -27,11 +13,10 @@
 
 (defn update-todo! [todo]
   (let [line-number (:id todo)
-        lines (modify-nth-line (read-all-lines) todo)] 
-    (with-open [w (io/writer file-name)]
-       (doseq [line lines]
-         (.write w line)
-         (.newLine w))))
+        lines (modify-nth-line (read-lines file-name) todo)
+        temp-file (File/createTempFile "todo" nil)] 
+    (write-lines temp-file lines)
+    (atomic-move temp-file file-name))
   todo)
 
 (defn add-todo! [todo] 
