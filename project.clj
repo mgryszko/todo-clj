9fb827e491a763d25d254d1cfb61b5240bf50740
mgryszko <marcin@grysz.com>
2015-03-01 22:04:31 +0100

    next version prepared

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject todo-clj "0.0.0"
+(defproject todo-clj "0.1.0-SNAPSHOT"
   :description "todo app"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "1.0.0"]
                  [clj-stacktrace "0.2.8"]]
-  :plugins [[lein-environ "1.0.0"]
-            [lein-release "1.0.5"]]
+  :plugins [[lein-environ "1.0.0"]]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.1.1"]
