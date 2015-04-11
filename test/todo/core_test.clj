4fdcb4f9baa6fccac503309841b83a59ae57dbd9
mgryszko <marcin@grysz.com>
2015-04-11 15:40:12 +0200

    Facts grouped; shared fixture as defs

--- test/todo/core_test.clj
+++ test/todo/core_test.clj
@@ -4,78 +4,82 @@
 
 (declare save-todo! delete-todo! id-exists? find-todo)
 
+(def any-task "any task")
+(def any-short-task "a")
+(def empty-task "")
+(def blank-task " \t\n\r ")
+(def existing-id 1)
+(def any-persistent-todo {:id 1 :task any-task})
+(def non-existing-id 2)
+
 (facts "todo use cases"
-  (let [any-task "any task"
-        any-short-task "a"
-        empty-task ""
-        blank-task " \t\n\r "
-        existing-id 1
-        any-persistent-todo {:id 1 :task any-task}
-        non-existing-id 2]
-    (facts "add todo"
-      (fact "empty todo cannot be added"
-        (can-todo-be-added? any-task) => [:ok]
-        (can-todo-be-added? any-short-task) => [:ok]
-        (can-todo-be-added? empty-task) => [:task-empty]
-        (can-todo-be-added? blank-task) => [:task-empty])
 
-      (fact "if still tried to be added, an exception is thrown"
-        (add-todo save-todo! empty-task) => (throws IllegalArgumentException))
+  (facts "add todo"
 
-      (fact "non-empty todo is added"
-        (add-todo save-todo! any-task) => any-persistent-todo
-          (provided
-            (save-todo! {:task any-task}) => any-persistent-todo :times 1)))
+    (fact "empty todo cannot be added"
+      (can-todo-be-added? any-task) => [:ok]
+      (can-todo-be-added? any-short-task) => [:ok]
+      (can-todo-be-added? empty-task) => [:task-empty]
+      (can-todo-be-added? blank-task) => [:task-empty])
 
-    (facts "update todo"
-      (fact "todo is updated"
-        (against-background (id-exists? (any-persistent-todo :id)) => true)
-        (update-todo id-exists? save-todo! any-persistent-todo) => any-persistent-todo
-          (provided
-            (save-todo! any-persistent-todo) => any-persistent-todo :times 1))
+    (fact "if still tried to be added, an exception is thrown"
+      (add-todo save-todo! empty-task) => (throws IllegalArgumentException))
 
-      (fact "empty todo cannot be updated"
-        (against-background (id-exists? existing-id) => true)
-        (can-todo-be-updated? id-exists? any-persistent-todo) => [:ok]
-        (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] any-short-task)) => [:ok]
-        (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] empty-task)) => [:task-empty]
-        (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] blank-task)) => [:task-empty])
+    (fact "non-empty todo is added"
+      (add-todo save-todo! any-task) => any-persistent-todo
+        (provided (save-todo! {:task any-task}) => any-persistent-todo :times 1)))
 
-      (fact "non-existing todo id cannot be updated"
-         (can-todo-be-updated? id-exists? {:id non-existing-id :task any-task}) => [:id-not-found non-existing-id]
-           (provided (id-exists? non-existing-id) => false))
+  (facts "update todo"
 
-      (fact "if still tried to be updated, an exception is thrown"
-         (against-background (id-exists? non-existing-id) => false)
-         (update-todo id-exists? save-todo! {:id non-existing-id :task any-task}) =>
-           (throws IllegalArgumentException)))
+    (fact "todo is updated"
+      (against-background (id-exists? (any-persistent-todo :id)) => true)
+      (update-todo id-exists? save-todo! any-persistent-todo) => any-persistent-todo
+        (provided (save-todo! any-persistent-todo) => any-persistent-todo :times 1))
 
-    (facts "delete todo"
-      (fact "todo is deleted"
-        (delete-todo id-exists? delete-todo! existing-id) => any-persistent-todo
-          (provided
-            (id-exists? existing-id) => true
-            (delete-todo! existing-id) => any-persistent-todo :times 1))
-      
-      (fact "todo without an id cannot be deleted"
-        (can-todo-be-deleted? id-exists? nil) => [:id-mandatory]
-        (can-todo-be-deleted? id-exists? false) => [:id-mandatory])
+    (fact "empty todo cannot be updated"
+      (against-background (id-exists? existing-id) => true)
+      (can-todo-be-updated? id-exists? any-persistent-todo) => [:ok]
+      (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] any-short-task)) => [:ok]
+      (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] empty-task)) => [:task-empty]
+      (can-todo-be-updated? id-exists? (assoc-in any-persistent-todo [:task] blank-task)) => [:task-empty])
 
-      (fact "non-existing todo id cannot be deleted"
-        (can-todo-be-deleted? id-exists? non-existing-id) => [:id-not-found non-existing-id]
-          (provided (id-exists? non-existing-id) => false))
+    (fact "non-existing todo id cannot be updated"
+       (can-todo-be-updated? id-exists? {:id non-existing-id :task any-task}) => [:id-not-found non-existing-id]
+         (provided (id-exists? non-existing-id) => false))
 
-      (fact "if still tried to be deleted, an exception is thrown"
-         (against-background (id-exists? non-existing-id) => false)
-         (delete-todo id-exists? delete-todo! non-existing-id) => (throws IllegalArgumentException)))
+    (fact "if still tried to be updated, an exception is thrown"
+       (against-background (id-exists? non-existing-id) => false)
+       (update-todo id-exists? save-todo! {:id non-existing-id :task any-task}) =>
+         (throws IllegalArgumentException)))
 
-    (facts "finds todos"
-      (fact "all todos"
-        (find-all-todos find-todo) => [any-persistent-todo]
-          (provided (find-todo) => [any-persistent-todo]))
+  (facts "delete todo"
 
-      (fact "todo by id"
-        (let [id (:id any-persistent-todo)]
-        (find-todo-by-id find-todo id) => any-persistent-todo
-          (provided (find-todo id) => any-persistent-todo))))))
+    (fact "todo is deleted"
+      (delete-todo id-exists? delete-todo! existing-id) => any-persistent-todo
+        (provided
+          (id-exists? existing-id) => true
+          (delete-todo! existing-id) => any-persistent-todo :times 1))
+    
+    (fact "todo without an id cannot be deleted"
+      (can-todo-be-deleted? id-exists? nil) => [:id-mandatory]
+      (can-todo-be-deleted? id-exists? false) => [:id-mandatory])
+
+    (fact "non-existing todo id cannot be deleted"
+      (can-todo-be-deleted? id-exists? non-existing-id) => [:id-not-found non-existing-id]
+        (provided (id-exists? non-existing-id) => false))
+
+    (fact "if still tried to be deleted, an exception is thrown"
+       (against-background (id-exists? non-existing-id) => false)
+       (delete-todo id-exists? delete-todo! non-existing-id) => (throws IllegalArgumentException)))
+
+  (facts "finds todos"
+         
+    (fact "all todos"
+      (find-all-todos find-todo) => [any-persistent-todo]
+        (provided (find-todo) => [any-persistent-todo]))
+
+    (fact "todo by id"
+      (let [id (:id any-persistent-todo)]
+      (find-todo-by-id find-todo id) => any-persistent-todo
+        (provided (find-todo id) => any-persistent-todo)))))
 
--- test/todo/infrastructure/file/repository_test.clj
+++ test/todo/infrastructure/file/repository_test.clj
@@ -6,54 +6,71 @@
             [todo.infrastructure.file.repository :refer :all]
             [todo.infrastructure.file.test-operations :refer :all]))
 
-(background (before :facts (delete-todo-file))
-            (after :facts (delete-todo-file)))
+(background (before :facts (delete-todo-file)
+                    :after (delete-todo-file)))
 
 (facts "file repository"
-  (fact "adds single todo"
-    (add-todo! {:task "any"}) =>
-      (every-checker (file-saved ["any"])
-                     {:id 1 :task "any"}))
 
-  (fact "adds two todos"
-    (add-todo! {:task "first"})
-    (add-todo! {:task "second"}) =>
-      (every-checker (file-saved ["first", "second"])
-                                 {:id 2 :task "second"}))
+  (facts "adds todo"
+    (fact "single todo"
+      (add-todo! {:task "any"}) =>
+        (every-checker (file-saved ["any"])
+                       {:id 1 :task "any"}))
+
+    (fact "two todos"
+      (add-todo! {:task "first"})
+      (add-todo! {:task "second"}) =>
+        (every-checker (file-saved ["first", "second"])
+                                 {:id 2 :task "second"})))
 
   (against-background 
     [(before :facts [(add-todo! {:task "first"})
                      (add-todo! {:task "second"})
                      (add-todo! {:task "third"})
                      (add-todo! {:task "fourth"})])]
+    (facts "updates todo"
+
+      (fact "first todo"
+        (let [todo {:id 1 :task "updated"}]
+          (update-todo! todo) =>
+            (every-checker (file-saved ["updated" "second" "third" "fourth"]) todo)))
+
+      (fact "last todo"
+        (let [todo {:id 4 :task "updated"}]
+          (update-todo! todo) =>
+            (every-checker (file-saved ["first" "second" "third" "updated"]) todo))))
+
+    (facts "deletes todo"
+
+      (fact "first todo"
+        (let [line-num 1]
+          (delete-todo! line-num) =>
+            (every-checker (file-saved ["second" "third" "fourth"])
+                           {:id line-num :task "first"})))
+
+      (fact "in-between todo"
+        (let [line-num 2]
+          (delete-todo! line-num) =>
+            (every-checker (file-saved ["first" "third" "fourth"])
+                           {:id line-num :task "second"})))
       
-    (fact "updates first todo"
-      (let [todo {:id 1 :task "updated"}]
-        (update-todo! todo) =>
-          (every-checker (file-saved ["updated" "second" "third" "fourth"]) todo)))
+      (fact "last todo"
+        (let [line-num 4]
+          (delete-todo! line-num) =>
+            (every-checker (file-saved ["first" "second" "third"])
+                           {:id line-num :task "fourth"})))) 
+    (facts "finds todos"
 
-    (fact "updates last todo"
-      (let [todo {:id 4 :task "updated"}]
-        (update-todo! todo) =>
-          (every-checker (file-saved ["first" "second" "third" "updated"]) todo)))
-    
-    (fact "deletes first todo"
-      (let [line-num 1]
-        (delete-todo! line-num) =>
-          (every-checker (file-saved ["second" "third" "fourth"])
-                         {:id line-num :task "first"})))
+      (fact "all"
+        (find-all) => [{:id 1 :task "first"}
+                       {:id 2 :task "second"}
+                       {:id 3 :task "third"}
+                       {:id 4 :task "fourth"}])
 
-    (fact "deletes in-between todo"
-      (let [line-num 2]
-        (delete-todo! line-num) =>
-          (every-checker (file-saved ["first" "third" "fourth"])
-                         {:id line-num :task "second"})))
-    
-    (fact "deletes last todo"
-      (let [line-num 4]
-        (delete-todo! line-num) =>
-          (every-checker (file-saved ["first" "second" "third"])
-                         {:id line-num :task "fourth"})))
+      (fact "by line number"
+        (find-by-line-number 1) => {:id 1 :task "first"}
+        (find-by-line-number 3) => {:id 3 :task "third"}
+        (find-by-line-number 4) => {:id 4 :task "fourth"}))
 
     (fact "checks if a line number exists"
       (line-num-exists? 0) => false
@@ -61,15 +78,4 @@
       (line-num-exists? 4) => true
       (line-num-exists? 5) => false
       (line-num-exists? "1") => false
-      (line-num-exists? "one") => false)
-
-    (fact "finds all todos"
-      (find-all) => [{:id 1 :task "first"}
-                     {:id 2 :task "second"}
-                     {:id 3 :task "third"}
-                     {:id 4 :task "fourth"}])
-
-    (fact "finds todo by line number"
-      (find-by-line-number 1) => {:id 1 :task "first"}
-      (find-by-line-number 3) => {:id 3 :task "third"}
-      (find-by-line-number 4) => {:id 4 :task "fourth"})))
+      (line-num-exists? "one") => false)))
