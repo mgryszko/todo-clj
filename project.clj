667b630828ab75c7727a519556f37c683b454741
mgryszko <marcin@grysz.com>
2015-04-13 22:52:19 +0200

    cli-stacktrace only for development

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "todo app"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "1.0.0"]
-                 [clj-stacktrace "0.2.8"]
                  [liberator "0.12.2"]
                  [compojure "1.3.2"]
                  [ring/ring-core "1.3.2"]]
@@ -10,6 +9,7 @@
             [lein-ring "0.9.2"]]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]
+                                  [clj-stacktrace "0.2.8"]
                                   [clj-http "1.0.1"]
                                   [ring-server "0.4.0"]]
                    :plugins [[lein-midje "3.1.1"]
